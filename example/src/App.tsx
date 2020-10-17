267bd5129c81f2026d097ae2efe509b5b0a7e237
Andre <andrersfcosta@gmail.com>
2020-10-17 17:52:49 -0300

    feat(init): add first structure

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'react-awesome-mouse-selector'
-import 'react-awesome-mouse-selector/dist/index.css'
+import Selection from 'react-awesome-mouse-selector'
+// import 'react-awesome-mouse-selector/dist/index.css'
+
+import './index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [elRefs, setElRefs] = useState<HTMLDivElement[]>([])
+  const [currentRef, setCurrentRef] = useState<HTMLDivElement>()
+
+  const addElementRef = (ref: any) => {
+    if (!elRefs.includes(ref)) {
+      const elRefsLet = elRefs
+      elRefsLet.push(ref)
+      setElRefs(elRefsLet)
+    }
+  }
+
+  const renderSelection = () => {
+    if (!currentRef || !elRefs) {
+      return null
+    }
+
+    return (
+      <Selection
+        target={currentRef}
+        elements={elRefs}
+        onSelectionChange={(e) => console.log(e)}
+        onHighlightChange={(e) => console.log(e)}
+        style='block'
+        dataContent={''}
+        blockSelection={false}
+        cellSize={{ width: 141, height: 98 }}
+        selectionSize={{ maxWidth: 422, maxHeight: 293 }}
+        blockedPoints={[]}
+      />
+    )
+  }
+
+  return (
+    <div
+      ref={(ref) => {
+        ref && setCurrentRef(ref)
+      }}
+      style={{ display: 'flex', maxWidth: '423px', flexWrap: 'wrap' }}
+    >
+      {Array(9)
+        .fill(null)
+        .map((_, index) => {
+          return (
+            <div
+              ref={addElementRef}
+              style={{
+                minWidth: '141px',
+                minHeight: '98px',
+                boxSizing: 'border-box',
+                border: '1px solid  black',
+                backgroundColor: 'red'
+              }}
+            >
+              {index}
+            </div>
+          )
+        })}
+      {renderSelection()}
+    </div>
+  )
 }
 
 export default App

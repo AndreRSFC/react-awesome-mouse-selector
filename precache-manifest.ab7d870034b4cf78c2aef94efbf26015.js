20b0b50dc6f02c5943e72b7c2d3c42694ee7b6d0
Andre <andrersfcosta@gmail.com>
2020-10-17 18:05:58 -0300

    Updates

---
+++ precache-manifest.ab7d870034b4cf78c2aef94efbf26015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294014cf1cc63451b75f5c8b4f05f765",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b416015a403f2371d4d",
+    "url": "./static/css/main.3cc9b5b6.chunk.css"
+  },
+  {
+    "revision": "7a4e401c1ca24292c316",
+    "url": "./static/js/2.9dfcc85b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9dfcc85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b416015a403f2371d4d",
+    "url": "./static/js/main.b9773c52.chunk.js"
+  },
+  {
+    "revision": "22b0d9271adbf5d2c9e9",
+    "url": "./static/js/runtime-main.dd97de0d.js"
+  }
+]);
